0082ea372abe90efe2eba53696bb2791b73ec898
Denis Czaplia <zgibex333@gmail.com>
2021-07-01 22:52:01 +0300

    test gh pages

---
+++ script/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.reviews-slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: false,
+    })
+})
